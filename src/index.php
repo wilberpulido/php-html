a744a7865ba322228b6df21fa3f888a8365f075f
wilberpulido <pulidowilber@gmail.com>
2024-01-29 19:18:16 -0300

    Condiciones para comenzar

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HTML en PHP</title>
+</head>
+<body>
+  <h1>Ojito</h1>
+  <?php $a='TEXTO' ?>
+
+  <?php echo "<p>Esto es un parrafo renderizado con php...</p>"; ?>
+  <?php echo $a ?>
+
+</body>
+</html>
