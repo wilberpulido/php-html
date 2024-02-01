b4c40b03ac7c19775a94abc2cd092f98e4a4ae94
wilberpulido <pulidowilber@gmail.com>
2024-02-01 08:29:56 -0300

    Obtener una solicitud en el servidor php

---
+++ src/test-request.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test</title>
+</head>
+<body>
+  
+<script>
+  const formData = new FormData();
+
+  formData.append('nombre','Wilber');
+  formData.append('edad','29');
+
+  fetch ("server.php?color=green",{
+    body: formData,
+    method: "POST"
+  })
+  .then(res => res.text())
+  .then(data=>{
+    console.log(data);
+  });
+
+</script>
+</body>
+</html>
